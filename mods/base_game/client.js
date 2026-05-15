12c760332101ac52e521a749284ad95fd10b3e97
Eyeseminal <148734630+stanleyzihanfan@users.noreply.github.com>
2026-05-15 10:15:02 -0700

    save commit

--- mods/base_game/client.js
+++ mods/base_game/client.js
@@ -47,5 +47,7 @@ function block_remove(params) {
 }
 
 // Register handlers with frontend dispatch table
-wsDispatch["block_add"] = block_add;
-wsDispatch["block_remove"] = block_remove;
+// wsDispatch["block_add"] = block_add;
+// wsDispatch["block_remove"] = block_remove;
+registerHandler("block_add",block_add);
+registerHandler("block_remove",block_remove);
