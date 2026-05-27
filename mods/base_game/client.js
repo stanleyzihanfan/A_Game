7cc19361a6bf1b71c2963379575bbc568fdeba82
Eyeseminal <148734630+stanleyzihanfan@users.noreply.github.com>
2026-05-27 12:43:20 -0700

    WIP

--- mods/base_game/client.js
+++ mods/base_game/client.js
@@ -46,6 +46,11 @@ function block_remove(params) {
     });
 }
 
+//Trigger on player move
+function notifyOnPlayerMove(params) {
+    
+}
+
 // Register handlers with frontend dispatch table
 // wsDispatch["block_add"] = block_add;
 // wsDispatch["block_remove"] = block_remove;
