123596af49973952b1b3f398b2ae0f6971d85c54
Eyeseminal <stanleyzihanfan@gmail.com>
2026-09-01 16:14:01 -0700

    save commit

--- mods/player_position/main.py
+++ mods/player_position/main.py
@@ -4,8 +4,10 @@ def testclient_server(gamestate,registry):
 def testserver_client(gamestate,registry):
     if not gamestate.exists(["sync_with_client"]):
         return
-    cursync=gamestate.get(["sync_with_client"])
-    cursync[]
+    with gamestate._lock:
+        cursync=gamestate.get(["sync_with_client"])
+        cursync["player_position:testsend"]="Sent from Server"
+        gamestate.set(["sync_with_client"])
 
 def register(registry):
     registry.register_handler("core:tick_hook",testclient_server,"player_position:client-server-test")
